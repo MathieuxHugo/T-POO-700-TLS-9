2a0c096da4563562ea6b246ae8aab06ad941f3ea
roux_ <pierre.roux@outlook.fr>
2021-10-26 11:38:24 +0200

    Fix Users V3

--- api/lib/api_web/views/users_view.ex
+++ api/lib/api_web/views/users_view.ex
@@ -3,11 +3,11 @@ defmodule TodolistWeb.UsersView do
   alias TodolistWeb.UsersView
 
   def render("index.json", %{users: users}) do
-    %{data: render_many(users, UserView, "users.json")}
+    %{data: render_many(users, UsersView, "users.json")}
   end
 
   def render("show.json", %{users: users}) do
-    %{data: render_one(users, UserView, "users.json")}
+    %{data: render_one(users, UsersView, "users.json")}
   end
 
   def render("users.json", %{users: users}) do
